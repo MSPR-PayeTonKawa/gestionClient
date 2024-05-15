689e4adafe20e31ad379bd8ec422c3577210c5a8
thomasBarille-ab <thomasbarille@gmail.com>
2024-05-15 15:22:02 +0200

    datasource, model, etc

---
+++ src/datasources/db.datasource.ts
@@ -0,0 +1,29 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'db',
+  connector: 'postgresql',
+  url: '',
+  host: 'localhost',
+  port: 5432,
+  user: 'dbuser',
+  password: 'admin',
+  database: 'dbClient',
+};
+
+@lifeCycleObserver('datasource')
+export class DbDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
+  static dataSourceName = 'db';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.db', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
