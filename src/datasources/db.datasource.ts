3b4ae94b7270b7e5a1ee11e032f60fa34a241c4a
Germain-L <germain.leignel@gmail.com>
2024-06-05 14:47:35 +0200

    fix: test

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -5,18 +5,17 @@ const config = {
   name: 'db',
   connector: 'postgresql',
   url: '',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.POSTGRES_USER || 'dbUser',
-  password: process.env.POSTGRES_PASSWORD || 'admin',
-  database: process.env.POSTGRES_DB || 'dbClient',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: process.env.DB_PORT ?? 5432,
+  user: process.env.POSTGRES_USER ?? 'dbUser',
+  password: process.env.POSTGRES_PASSWORD ?? 'admin',
+  database: process.env.POSTGRES_DB ?? 'dbClient',
 };
 
 @lifeCycleObserver('datasource')
 export class DbDataSource
   extends juggler.DataSource
-  implements LifeCycleObserver
-{
+  implements LifeCycleObserver {
   static dataSourceName = 'db';
   static readonly defaultConfig = config;
 
